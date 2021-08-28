8bf38d3fb95f4897a8138d75dd99847ba71fd8a5
kyunghwanma <ghma1213@gmail.com>
2021-08-28 18:51:50 +0900

    3장 완료

--- src/main/java/com/magh/springboot/service/posts/PostsService.java
+++ src/main/java/com/magh/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.magh.springboot.service.posts;
 
+import com.magh.springboot.domain.posts.Posts;
 import com.magh.springboot.domain.posts.PostsRepository;
+import com.magh.springboot.web.dto.PostsResponseDto;
 import com.magh.springboot.web.dto.PostsSaveRequestDto;
+import com.magh.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestsDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestsDto.getTitle(), requestsDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. Id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
