24dff451886d5f003652e9d4225a0cfca465f25f
kyunghwanma <ghma1213@gmail.com>
2021-08-31 22:46:47 +0900

    리액트 연결

--- src/main/java/com/magh/springboot/web/HelloController.java
+++ src/main/java/com/magh/springboot/web/HelloController.java
@@ -1,14 +1,18 @@
 package com.magh.springboot.web;
 
 import com.magh.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
+
         return "hello";
     }
 
@@ -17,4 +21,12 @@ public class HelloController {
 
         return new HelloResponseDto(name, amount);
     }
+
+    @CrossOrigin("*")
+    @GetMapping("/api/hello")
+    public String hello_react(){
+
+        return "안녕하세요. 현재 서버시간은 " + new Date() + "입니다. \n";
+    }
+
 }
