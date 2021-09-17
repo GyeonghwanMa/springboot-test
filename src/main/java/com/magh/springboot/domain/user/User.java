3689aa1a76d227141205727e123941374d0bfa08
Gyeonghwan <ghma1213@gmail.com>
2021-09-17 10:10:19 +0900

    user 도메인 추가중

---
+++ src/main/java/com/magh/springboot/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.magh.springboot.domain.user;
+
+import com.magh.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+//    private Role role;
+
+}
