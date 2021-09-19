2bef152c23fd43f3443ff3ad4beffc94e5b6ca2b
kyunghwanma <ghma1213@gmail.com>
2021-09-20 00:44:36 +0900

    구글 로그인

--- src/main/java/com/magh/springboot/domain/user/User.java
+++ src/main/java/com/magh/springboot/domain/user/User.java
@@ -24,8 +24,26 @@ public class User extends BaseTimeEntity {
     @Column
     private String picture;
 
-//    @Enumerated(EnumType.STRING)
-//    @Column(nullable = false)
-//    private Role role;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
